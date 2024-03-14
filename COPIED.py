3b5b285971e426b3f381f18fa10fea171bdb0bcc
D4niel0s <das.volkov@gmail.com>
2024-03-14 21:15:29 +0200

    Tried to make this fit our needs

--- COPIED.py
+++ COPIED.py
@@ -5,10 +5,7 @@
 import websocket # pip install websocket-client
 import numpy as np
 import matplotlib.pyplot as plt
- 
-# Image dimensions - this needs to match our setting on the ESP32 CAM board
-IMG_X = 320 # QVGA 320x240
-IMG_Y = 240
+from PIL import Image
 
 ws = websocket.WebSocket()
 ws.connect("ws://192.168.xx.xxx") # Use the IP address from the ESP32 board - printed to the serial monitor
@@ -25,26 +22,11 @@ while(1):
     # Wait for server to respond and print it
     if (str == "capture"):
         binResp = ws.recv_frame() # receiving binary image data (we assume grey scale) from camera
-  
-        # we are going to store the received binary image data in an array... 
-        img_array = np.zeros((IMG_Y,IMG_X))
-        x = 0
-        y = 0
-        count = 0
+        binDat = bytearray(binResp.data)
 
-        # traverse the captured data one byte at a time and populate image array
-        for byte in bytearray(binResp.data):
-           #print (byte)
-           y = count // IMG_X
-           x = count % IMG_X
-           img_array[y,x] = byte
-           count = count + 1
+        IMG = Image.frombytes(mode='RGBA', size=len(binDat), data=binDat, decoder_name='raw')
 
-        # display captured image
-        plt.figure('Capture Image 1')
-        plt.imshow(img_array)
-        plt.set_cmap('gray')
-        plt.show(block=False)
+        IMG.save("stream.jpg")
 
     else:
        result = ws.recv()
