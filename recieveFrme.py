1f63d4ab0354920fd186be58d58be0710bb10470
D4niel0s <das.volkov@gmail.com>
2024-03-20 00:59:48 +0200

    Added a threshold to the refresh rate

--- recieveFrme.py
+++ recieveFrme.py
@@ -3,25 +3,34 @@
 # Reference... https://shawnhymel.com/1675/arduino-websocket-server-using-an-esp32/
 # also https://www.dfrobot.com/blog-1194.html?tracking=5cc027fb1dc4e
 import websocket # pip install websocket-client
-from datetime import timedelta
-from datetime import datetime
+from datetime import datetime, timedelta
 
 ws = websocket.WebSocket()
 ws.connect("ws://192.168.200.126") # Use the IP address from the ESP32 board - printed to the serial monitor
 
-while(1):
-    now = datetime.now()
-    prev = now
 
-    ws.send("capture") # Triggers onWebSocketEvent()
+def main():
+    threshhold = timedelta(seconds=0.05)
+
+    recieve_frame()
+    lastRecieved = datetime.now()
+
+    while(1):
+        delta = datetime.now() - lastRecieved
+        if(delta >= threshhold):
+            recieve_frame()
+            lastRecieved = datetime.now()
+            print(delta)
+
+
+def recieve_frame():
+    ws.send("") # Triggers onWebSocketEvent() of TEXT type
 
     binResp = ws.recv() # receiving binary image data from camera
 
     with open("stream.jpg", 'wb') as f:
         f.write(binResp)
-        
-    delta = now-prev
-    print(delta)
 
-# Gracefully close WebSocket connection (Will never get here, connection will be abruptly closed when code is stopped)
-ws.close()
+
+if(__name__ == '__main__'):
+    main()
