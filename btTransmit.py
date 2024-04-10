8444f6a8264d678bb989a61d6441368423647076
D4niel0s <das.volkov@gmail.com>
2024-04-10 23:18:24 +0300

    Compacted empty Lines

--- btTransmit.py
+++ btTransmit.py
@@ -1,14 +1,11 @@
 import serial
 
 port = "COM10"
-
 s = serial.Serial(port, 9600)
 
 s.flushInput()
 
-
 inp = int(input("choose song:\t")) #In the real script, this variable will be the output of the classification
-
 s.write(inp.to_bytes())
-    
+
 s.close()
