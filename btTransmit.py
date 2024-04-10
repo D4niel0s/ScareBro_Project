b6c0e323713159fc197b6d455181dd2f98e922a3
D4niel0s <das.volkov@gmail.com>
2024-04-10 23:28:08 +0300

    Changed script to work

--- btTransmit.py
+++ btTransmit.py
@@ -5,7 +5,7 @@ s = serial.Serial(port, 9600)
 
 s.flushInput()
 
-inp = int(input("choose song:\t")) #In the real script, this variable will be the output of the classification
-s.write(inp.to_bytes())
+inp = input("choose song:\t").encode() #In the real script, this variable will be the output of the classification
+s.write(inp) #inp is of type bytes (encoded string)
 
 s.close()
